7bd7f435be413c6b1fca5e364c80b1346e40bee8
Ritvik Shukla <ritvikshukla261@gmail.com>
2023-04-23 04:05:43 +0530

    init

---
+++ bash-colors/color.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${RED}This is some red text, ${ENDCOLOR}"
+echo -e "${GREEN}And this is some green text${ENDCOLOR}"
